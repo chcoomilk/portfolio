0eb1c3e8ae3948c3f33815334a58b29350edfa63
chcoomilk <klaustin.wijaya@gmail.com>
2023-01-08 16:05:32 +0700

    update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://klaustinw.github.io',
+  site: 'https://chcoomilk.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
   server: {
     port: +process.env.PORT || undefined,
