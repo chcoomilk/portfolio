ba4f959cb33e21df91ce3a0bcee61a263bd43bd3
chcoomilk <klaustin.wijaya@gmail.com>
2023-01-15 13:54:03 +0700

    update links and such

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chcoomilk.github.io',
-  base: "/porto",
+  base: "/portfolio",
   integrations: [mdx(), sitemap(), tailwind()],
   server: {
     port: +process.env.PORT || undefined,
