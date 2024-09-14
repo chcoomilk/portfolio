4a6c95e9e4dbe5eef3dc32479e6a247a212787d2
chcoomilk <klaustin.wijaya@gmail.com>
2024-09-14 21:39:04 +0700

    update social media links

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,8 @@
 export const SITE_TITLE = 'Klaustin';
 export const SITE_DESCRIPTION = "Klaustin Wijaya's website portfolio, a software engineer from HACKTIV8. No this is not a template, I wrote this myself. Visit this website if you're interested and want to hire me.";
 export const LINKS = {
-    github: "https://github.com/chcoomilk",
-    instagram: "https://instagram.com/klaudtib",
-    steam: "https://steamcommunity.com/id/o2i3_/",
+    github: "https://gitlab.com/klaustin",
+    instagram: "https://instagram.com/klaustinig",
     linkedin: "https://www.linkedin.com/in/klaustin-wijaya/",
     whatsapp: "https://wa.me/qr/F7SWYLOMI5YKB1",
 };
