ec4477aae645f8413f89772292a1540f4d58fde2
chcoomilk <klaustin.wijaya@gmail.com>
2024-09-15 10:34:54 +0700

    update description and introductory less sad

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Klaustin';
-export const SITE_DESCRIPTION = "Klaustin Wijaya's website portfolio, a software engineer from HACKTIV8. No this is not a template, I wrote this myself. Visit this website if you're interested and want to hire me.";
+export const SITE_TITLE = "Klaustin's Portfolio";
+export const SITE_DESCRIPTION = "Klaustin Wijaya's work portfolio website, a software engineer. Currently working at TalentMesh";
 export const LINKS = {
     github: "https://gitlab.com/klaustin",
     instagram: "https://instagram.com/klaustinig",
