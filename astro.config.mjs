7bd01a44046f097311837636491687a419566c95
klaustin <klaustin.wijaya@gmail.com>
2025-02-07 03:00:11 +0800

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://klaustinw.gitlab.io',
-  base: "/portfolio",
+  site: process.env.SITE_URL,
+  base: "/portfolio/",
   integrations: [mdx(), sitemap(), tailwind()],
   server: {
     port: +process.env.PORT || undefined,
   },
-  outDir: 'public',
-  publicDir: 'static',
+  outDir: "./public",
+  publicDir: "./static",
 });
