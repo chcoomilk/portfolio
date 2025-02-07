7fb4d30a6af3776e0015b0cbd9a354973174025f
klaustin <klaustin.wijaya@gmail.com>
2025-02-07 13:10:00 +0800

    fix rss
    fix env port dev not getting retrieved

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const { SITE_URL, PORT } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: SITE_URL,
   base: "/portfolio",
   integrations: [mdx(), sitemap(), tailwind()],
-  server: {
-    port: +process.env.PORT || undefined,
-  },
+  server: { port: +PORT || undefined },
   publicDir: "./static",
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
+export const GET = async (context) =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./projects/**/*.md'),
+		site: context.site,
+		items: await pagesGlobToRssItems(import.meta.glob('./projects/**/*.md')),
 	});
