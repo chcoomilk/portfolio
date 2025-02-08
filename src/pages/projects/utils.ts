92c38bc114c70ae9c9d849315bae8a747deb0529
klaustin <klaustin.wijaya@gmail.com>
2025-02-08 19:15:39 +0800

    update Astro glob to import meta glob with addition of type safety

---
+++ src/pages/projects/utils.ts
@@ -0,0 +1,14 @@
+import type { MarkdownInstance } from "astro";
+
+export type Project = MarkdownInstance<{
+    title: string;
+    description: string;
+    pubDate: string;
+    heroImage: string;
+    url: string;
+    tags: string[],
+    highlight: boolean;
+    preferred_position: number;
+}>;
+
+export const getProjects = () => Object.values(import.meta.glob<Project>("./*.{md,mdx}", { eager: true }));
